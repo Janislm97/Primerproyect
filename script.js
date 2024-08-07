d8aaf71a3fd12f34bab7027dca7b38ef618d68c0
Janis <jpsp.janis@gmail.com>
2024-08-07 17:02:05 -0400

    actualizacion

---
+++ script.js
@@ -0,0 +1,16 @@
+let datoString = "Mensaje"
+let datoNumber = 2024
+let datoBoolean = true
+
+let datoUndefined = undefined
+let datoNull = null
+
+let datoLista = [
+    "Cereal",
+    "Harina",
+    "Arroz",
+    "Pasta",
+]
+
+console.log(datoLista)
+
